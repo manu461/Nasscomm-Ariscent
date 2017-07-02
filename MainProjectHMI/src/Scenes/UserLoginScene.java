f439c82c76635317395a5577b1eef8f09f91168b
harshit2205 <harshit.arpit@gmail.com>
2017-07-02 23:32:28 +0530

    harshit
    comments added

--- MainProjectHMI/src/Scenes/UserLoginScene.java
+++ MainProjectHMI/src/Scenes/UserLoginScene.java
@@ -74,6 +74,7 @@ public class UserLoginScene {
 
         //----------------------------------->
         //--------------HELLO--------------->
+        //123456789
 
         managerLoginMenuItem.setOnAction(e -> ManagerLoginScene.passControl(window));
         aboutMenuItem.setOnAction(e -> {PopUpStage.show("About HMI", AboutScene.getScene());});

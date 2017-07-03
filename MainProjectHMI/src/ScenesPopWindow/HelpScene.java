e2d7a2fb6ee8393ce02a9fb06f153d05d3a0fcb8
manu461 <rastogimanu461@gmail.com>
2017-07-03 12:21:35 +0530

    help scene added

--- MainProjectHMI/src/ScenesPopWindow/HelpScene.java
+++ MainProjectHMI/src/ScenesPopWindow/HelpScene.java
@@ -4,6 +4,7 @@ import Utils.HyperText;
 import javafx.geometry.*;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
@@ -14,67 +15,70 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.*;
 
-import java.awt.*;
 
 public class HelpScene {
     public static Scene getScene(){
 
 
-        VBox labels = new VBox(20);
-        VBox textFields = new VBox(10);
-        HBox hBox = new HBox(20);
-        VBox vBox = new VBox(30);
+        VBox labelVBox = new VBox(10);
+        VBox textFieldVBox = new VBox(10);
+        HBox forumPaneWithoutButtonHBox = new HBox(20);
+        VBox forumPanelCompleteVBox = new VBox(10);
+        VBox allInVBox = new VBox(30);
         HBox facebookPane = new HBox(5);
-        HBox instaPane = new HBox(5);
+        HBox instagramPane = new HBox(5);
         HBox twitterPane = new HBox(5);
-        VBox links = new VBox(5);
-        links.setAlignment(Pos.BOTTOM_LEFT);
-        links.setPadding(new Insets(0,10,0,10));
-        hBox.setPadding(new Insets(10,10,10,10));
+        VBox linksVBox = new VBox(5);
+        linksVBox.setAlignment(Pos.TOP_LEFT);
+        linksVBox.setPadding(new Insets(0,10,0,10));
+        forumPaneWithoutButtonHBox.setPadding(new Insets(0,0,0,0));
 
-        Label lMail_id = new Label("e-mail :");
-        Label lDescription = new Label("Description :");
-        TextField tMail_id = new TextField();
-        tMail_id.setPromptText("enter your mail");
-        TextArea tDescription = new TextArea();
-        tDescription.setPrefColumnCount(20);
-        tDescription.setPrefRowCount(2);
-        tDescription.setPromptText("specify your problem ");
+        Label mailIdLabel = new Label("e-mail :");
+        Label descriptionLabel = new Label("Description :");
+        TextField mailTextField = new TextField();
+        mailTextField.setPromptText("enter your mail");
+        TextArea descriptionTextArea = new TextArea();
+        descriptionTextArea.setMaxWidth(280);
+        descriptionTextArea.setMaxHeight(100);
+        descriptionTextArea.setPromptText("specify your problem ");
 
-        Button facebookLink = new Button("FaceBook Link");
-        Button instagramLink = new Button("Instagram Link");
-        Button twitterLink = new Button("Twitter Link");
-        Button submitquery = new Button("Submit");
-        submitquery.getStyleClass().add("button-blue");
-        facebookLink.setOnAction(e -> HyperText.openWebpage("https://www.facebook.com/"));
-        instagramLink.setOnAction(e -> HyperText.openWebpage("https://www.instagram.com/"));
-        twitterLink.setOnAction(e -> HyperText.openWebpage("https://twitter.com/"));
-        submitquery.setOnAction(e -> System.out.println("mail and description succesfully submitted"));
+        Hyperlink gitHubDocumentationLink = new Hyperlink("https://github.com/HMI-Documentation");
+        Hyperlink gitHubForum = new Hyperlink("https://github.com/HMI-CommunityForum");
+        Hyperlink websiteForum = new Hyperlink("https://help-MI.in/forum");
+        Button submitQueryButton = new Button("Submit");
+        gitHubDocumentationLink.setOnAction(e -> HyperText.openWebpage("https://github.com/manu461/Nasscomm-Ariscent/tree/master/MainProjectHMI"));
+        gitHubForum.setOnAction(e -> HyperText.openWebpage("https://github.com/"));
+        websiteForum.setOnAction(e -> HyperText.openWebpage("https://twitter.com/"));
+        submitQueryButton.setOnAction(e -> {});
 
-        ImageView facebookIcon = new ImageView(new Image("/ImageResource/facebook_logo.png"));
-        facebookIcon.setFitWidth(13);
-        facebookIcon.setFitHeight(13);
-        ImageView instagramIcon = new ImageView(new Image("/ImageResource/instagram_logo.png"));
-        instagramIcon.setFitWidth(13);
-        instagramIcon.setFitHeight(13);
-        ImageView twitterIcon = new ImageView(new Image("/ImageResource/twitter_logo.png"));
-        twitterIcon.setFitWidth(13);
-        twitterIcon.setFitHeight(13);
+        ImageView forumLogoImageView = new ImageView(new Image("/ImageResource/github_logo.png"));
+        forumLogoImageView.setFitWidth(20);
+        forumLogoImageView.setFitHeight(20);
+        ImageView documentationLogoImageView = new ImageView(new Image("/ImageResource/github_logo.png"));
+        documentationLogoImageView.setFitWidth(20);
+        documentationLogoImageView.setFitHeight(20);
+        ImageView websiteForumLogoImageView = new ImageView(new Image("/ImageResource/forum_logo.png"));
+        websiteForumLogoImageView.setFitWidth(20);
+        websiteForumLogoImageView.setFitHeight(20);
 
-        facebookPane.getChildren().addAll(facebookIcon, facebookLink);facebookPane.setAlignment(Pos.CENTER_LEFT);
-        instaPane.getChildren().addAll(instagramIcon, instagramLink);instaPane.setAlignment(Pos.CENTER_LEFT);
-        twitterPane.getChildren().addAll(twitterIcon, twitterLink);twitterPane.setAlignment(Pos.CENTER_LEFT);
+        facebookPane.getChildren().addAll(forumLogoImageView, gitHubDocumentationLink);facebookPane.setAlignment(Pos.CENTER_LEFT);
+        instagramPane.getChildren().addAll(documentationLogoImageView, gitHubForum);instagramPane.setAlignment(Pos.CENTER_LEFT);
+        twitterPane.getChildren().addAll(websiteForumLogoImageView, websiteForum);twitterPane.setAlignment(Pos.CENTER_LEFT);
 
-        links.getChildren().addAll(facebookPane,instaPane,twitterPane);
+        linksVBox.getChildren().addAll(facebookPane,instagramPane,twitterPane);
 
-        labels.getChildren().addAll(lMail_id,lDescription);
-        textFields.getChildren().addAll(tMail_id,tDescription);
-        hBox.setStyle("-fx-background-color: #090300");
-        hBox.setAlignment(Pos.CENTER);
-        hBox.getChildren().addAll(labels,textFields);
-        vBox.setAlignment(Pos.CENTER);
-        vBox.getChildren().addAll(links,hBox,submitquery);
+        labelVBox.getChildren().addAll(mailIdLabel,descriptionLabel);
+        textFieldVBox.getChildren().addAll(mailTextField,descriptionTextArea);
 
-        return new Scene(vBox,400,350);
+        forumPaneWithoutButtonHBox.setAlignment(Pos.CENTER_LEFT);
+        forumPaneWithoutButtonHBox.getChildren().addAll(labelVBox,textFieldVBox);
+        forumPanelCompleteVBox.getChildren().addAll(forumPaneWithoutButtonHBox,submitQueryButton);
+        forumPanelCompleteVBox.setStyle("-fx-background-color: #000000");
+        forumPanelCompleteVBox.setAlignment(Pos.CENTER);
+        forumPanelCompleteVBox.setPadding(new Insets(10,10,10,10));
+        allInVBox.setAlignment(Pos.TOP_CENTER);
+        allInVBox.getChildren().addAll(linksVBox,forumPanelCompleteVBox);
+
+        return new Scene(allInVBox,400,350);
     }
 }
--- MainProjectHMI/src/Utils/HyperText.java
+++ MainProjectHMI/src/Utils/HyperText.java
@@ -1,11 +1,10 @@
 package Utils;
 
 import java.awt.*;
-import java.net.URISyntaxException;
 import java.net.URL;
 
 public class HyperText {
-    public static void openWebpage(String urlString) {
+    public static void openWebPage(String urlString) {
         try {
             Desktop.getDesktop().browse(new URL(urlString).toURI());
         } catch (Exception e) {
